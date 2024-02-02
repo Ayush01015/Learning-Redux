c485001e710d03d25506cee41c38a025177764a5
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2024-02-02 17:06:03 +0530

    traditional redux setup and useCase

---
+++ Chapter 1/Redux/index.js
@@ -0,0 +1,16 @@
+import {createStore} from "redux"
+
+
+//store
+const store = createStore(reducer)
+
+//reducer
+function reducer(state={amount:1},action){
+    return state;
+} 
+
+//global state
+console.log(store.getState());
+
+// store.dispatch()
+
