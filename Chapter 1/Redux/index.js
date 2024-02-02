3a5eeb654d6ef566fbd8392a25be3b8403f6c0ac
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2024-02-02 17:13:00 +0530

    dispatching an action

--- Chapter 1/Redux/index.js
+++ Chapter 1/Redux/index.js
@@ -1,16 +1,22 @@
-import {createStore} from "redux"
+/* Traditional Redux */
 
+import {createStore} from "redux"
 
 //store
 const store = createStore(reducer)
 
 //reducer
 function reducer(state={amount:1},action){
+    if(action.type === "increment"){
+        return {amount:state.amount+1}
+    }
     return state;
 } 
 
 //global state
 console.log(store.getState());
 
-// store.dispatch()
+//dispatching an action -> {type:"increment"}
+store.dispatch({type:"increment"})
 
+console.log(store.getState());
