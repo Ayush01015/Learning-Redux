09c09a67668c49494a3693cdb583554c3d2dd58c
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2024-02-04 04:10:52 +0530

    mutability in redux

--- Chapter 1/Redux/index.js
+++ Chapter 1/Redux/index.js
@@ -1,22 +1,31 @@
 /* Traditional Redux */
 
-import {createStore} from "redux"
+import { createStore } from "redux";
 
 //store
-const store = createStore(reducer)
-
+const store = createStore(reducer);
+const history = [];
 //reducer
-function reducer(state={amount:1},action){
-    if(action.type === "increment"){
-        return {amount:state.amount+1}
-    }
-    return state;
-} 
+function reducer(state = { amount: 1 }, action) {
+  if (action.type === "increment") {
+    //state.amount = state.amount + 1; wrong
+     return { amount: state.amount + 1 };
+  }
+  return state;
+}
 
 //global state
-console.log(store.getState());
+// console.log(store.getState());
+
+//subscribing
+store.subscribe(() => {
+  history.push(store.getState());
+  console.log(history);
+});
+
+setInterval(() => {
+  store.dispatch({ type: "increment" });
+}, 1500);
 
 //dispatching an action -> {type:"increment"}
-store.dispatch({type:"increment"})
-
-console.log(store.getState());
+// store.dispatch({type:"increment"})
