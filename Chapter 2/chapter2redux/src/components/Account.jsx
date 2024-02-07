57f2618ccced9b01f06109d22972702050c6e217
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2024-02-08 01:14:32 +0530

    fix/stateValue

--- Chapter 2/chapter2redux/src/components/Account.jsx
+++ Chapter 2/chapter2redux/src/components/Account.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 import "../App.css"
 const Account = () => {
-  const [Amount, setAmount] = useState(0);
+  const [account, setAccount] = useState({amount:0});
   const [incValue, setincValue] = useState(0);
 
   const handleIncrement = () => {
-    setAmount((prev) => prev + 1);
+    setAccount({amount:account.amount + 1});
   };
   const handleDecrement = () => {
-    setAmount((prev) => prev - 1);
+    setAccount({amount:account.amount - 1});
   };
-  const handleincValue = (incValue) => {
-    setAmount(prev => prev + incValue);
+  const handleIncValue = (incValue) => {
+    setAccount({amount:account.amount + incValue});
   };
 
   return (
     <div className="container"  >
       <h3>Account Component</h3>
-      <h4>Amount: ${Amount}</h4>
+      <h4>account: ${account.amount}</h4>
       <div
         style={{
           display: "flex",
@@ -31,7 +31,7 @@ const Account = () => {
           decrement -
         </button>
         <input value={incValue} onChange={(e) => setincValue(+(e.target.value))} />
-        <button onClick={()=>handleincValue(incValue)}>
+        <button onClick={()=>handleIncValue(incValue)}>
           Increment by {incValue} +
         </button>
       </div>
